1f7450df3d737da0fb0d762d91f09c3f0b7b5132
hirakirahi <hirakirahi@gmail.com>
2022-04-21 09:30:08 +0900

    update: Local environment db environment

---
+++ docker/db/initdb.d/000_test.sql
@@ -0,0 +1,33 @@
+-- 認証方式を変更
+use mysql;
+ALTER USER 'root'@'%' IDENTIFIED WITH mysql_native_password BY fuel;
+ALTER USER 'fuel'@'%' IDENTIFIED WITH mysql_native_password BY fuel;
+
+-- データベース指定
+USE fuel;
+
+-- テーブル初期化
+DROP TABLE IF EXISTS test;
+
+-- テーブル生成
+CREATE TABLE test (
+	id int NOT NULL AUTO_INCREMENT COMMENT 'id',
+	username varchar(127) DEFAULT NULL COMMENT '名前',
+	password varchar(63) DEFAULT NULL COMMENT 'パスワード',
+	reg_date datetime DEFAULT NULL COMMENT '登録日',
+	upd_date timestamp ON UPDATE CURRENT_TIMESTAMP COMMENT '更新日',
+	del_date datetime DEFAULT NULL COMMENT '削除日',
+	PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+
+-- デフォルトデータを追加
+INSERT INTO test (
+    username,
+    password,
+    reg_date
+)
+VALUES (
+	'webmonster',
+	'web',
+	'2022-04-21 00:00:00'
+);
